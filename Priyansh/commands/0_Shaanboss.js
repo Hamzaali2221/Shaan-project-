b5a8b30cfc319c1ac6427d9a6d9bcfc7fd5f9918
HamzaAli2221 <githuba838@gmail.com>
2025-07-08 00:00:23 +0500

    Update and rename 0_julmiboss.js to 0_Shaanboss.js

--- Priyansh/commands/0_Shaanboss.js
+++ Priyansh/commands/0_Shaanboss.js
@@ -3,7 +3,7 @@ module.exports.config = {
   name: "sorry",
     version: "1.1.1",
   hasPermssion: 0,
-  credits: "𝑱𝑼𝑳𝑴𝑰 𝑱𝑨𝑨𝑻", 
+  credits: "SHAAN KHAN", 
   description: "Just Respond",
   commandCategory: "no prefix",
     cooldowns: 5, 
@@ -12,9 +12,9 @@ module.exports.config = {
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
   var { threadID, messageID } = event;
   let react = event.body.toLowerCase();
-  if(react.includes("Me julmi hu BSDK") ||
-     react.includes("Tera malik hu") || react.includes("Tera boss hu julmi") || react.includes("Tera papa hu") ||
-react.includes("Tera boss hu julmi jaat") ||
+  if(react.includes("Me SHAN hu BSDK") ||
+     react.includes("Tera malik hu") || react.includes("Tera boss hu SHAAN") || react.includes("Tera papa hu") ||
+react.includes("Tera boss hu SHAAN jaat") ||
 react.includes("Malik se bckchodi")) {
     var msg = {
         body: `【  𝐒0𝐑𝐑𝐘 𝐌𝐀𝐋𝐈𝐊 𝐀𝐁 𝐆𝐀𝐋𝐓𝐈 𝐍𝐀𝐇𝐈 𝐊𝐀𝐑𝐔𝐍𝐆𝐀 𝐏𝐋𝐒 𝐀𝐊 𝐁𝐀𝐑 𝐌𝐀𝐅 𝐊𝐀𝐑 𝐃0______🤐😙😥】`,
